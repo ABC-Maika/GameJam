7d181de6dd2adb1b0a638eeecc1e6f330a3aeae0
OrlinStefanov <orlinss2008@gmail.com>
2026-02-20 12:14:20 +0200

    playerStats

---
+++ GameJam/Assets/Scripts/Player/PlayerStats.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerStats
+{
+    public float speed;
+    public float power;
+    public float health;
+    public float stamina;
+    public float sprint;
+}
